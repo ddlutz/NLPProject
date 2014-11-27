95bb43535b5952bac6e975d4c247b7efc651323b
Doug Dlutz <djd3681@rit.edu>
2014-11-27 15:11:59 -0500

    Using chi-squared for important features

--- project.py
+++ project.py
@@ -40,6 +40,8 @@ def generateVocabulary(tokenListList):
 
     pattern = re.compile(r'[a-z]+')
 
+    vocabCount = {}
+
     for tokenList in tokenListList:
         for token in tokenList:
             """retain only [a-z] in token"""
@@ -47,9 +49,12 @@ def generateVocabulary(tokenListList):
             if len(allMatches) > 0:
                 vocabWord = ''.join(allMatches)
                 vocab.append(vocabWord)
+                if vocabWord in vocabCount:
+                    vocabCount[vocabWord] = vocabCount[vocabWord] +1
+                else:
+                    vocabCount[vocabWord] = 1
 
-    return set(vocab)
-
+    return [w for w in vocabCount.keys()]
 
 
 def containsDate(tokenList):
@@ -99,6 +104,27 @@ def getFeatures(vocab, tokenList):
 
     return features
 
+def getFeaturesFiltered(vocab, tokenList, filters):
+    
+    features = {}
+
+    tokenUnigrams = generateVocabulary([tokenList])
+    for word in vocab:
+        if word in tokenUnigrams and (('Unigram-' + word) in filters):
+            features['Unigram-' + word] = 1
+        elif ('Unigram-' + word) in filters:
+            features['Unigram-' + word] = 0
+
+    if 'ContainsDate' in filters:
+        features['ContainsDate'] = containsDate(tokenList)
+    if 'numPuncutation' in filters:
+        features['numPunctuation'] = numPunctuation(tokenList)
+    if 'numChars' in filters:
+        features['numChars'] = numChars(tokenList)
+
+
+    return features
+
 def main():
 
     if len(sys.argv) < 3:
@@ -121,9 +147,6 @@ def main():
     hamFeaturedTrain = [ (getFeatures(vocab, h), 0) for h in hamProcessedTrain]
     spamFeaturedTrain = [ (getFeatures(vocab, s), 1) for s in spamProcessedTrain]
     
-    allTrainData = hamFeaturedTrain + spamFeaturedTrain
-    svmDataTrain = [d[0] for d in allTrainData]
-    svmAnsTrain = [d[1] for d in allTrainData]
 
     """Process TEST file """
     print "Processing test file"
@@ -136,7 +159,65 @@ def main():
 
     hamFeaturedTest = [ (getFeatures(vocab, h), 0) for h in hamProcessedTest]
     spamFeaturedTest = [ (getFeatures(vocab, s), 1) for s in spamProcessedTest]
+
+    numCorrect = 0
+    feature = None
+
+    allTrainData = hamFeaturedTrain + spamFeaturedTrain
+    svmDataTrain = [d[0] for d in allTrainData]
+    svmAnsTrain = [d[1] for d in allTrainData]
+
+    allTestData = hamFeaturedTest + spamFeaturedTest
+    svmDataTest = [d[0] for d in allTestData]
+    svmAnsTest = [d[1] for d in allTestData]
+
+    vec = DictVectorizer()
+    svmTrainDataVectored = vec.fit_transform(svmDataTrain).toarray()
+    svmTestDataVectored = vec.fit_transform(svmDataTest).toarray()
     
+    print 'lengths'
+    print len(svmTestDataVectored)
+    print len(svmTestDataVectored[0])
+    svmClass = svm.SVC(kernel = 'linear', gamma=0.0005, C = 600)
+    print "fitting SVM"
+    test_predict = svmClass.fit(svmTrainDataVectored, svmAnsTrain).predict(svmTestDataVectored)
+
+
+    from sklearn.feature_selection import chi2, SelectKBest
+    selector = SelectKBest(chi2).fit(svmTrainDataVectored, svmAnsTrain) # There is more to SelectKBest; cf. documentation
+    top10 = selector.scores_.argsort()[::-1][:10]
+    for f in top10:
+        print '%.3e\t%s' % (selector.pvalues_[f], vec.get_feature_names()[f])
+
+    w = svmClass.coef_
+    ws = sorted([abs(q) for q in w[0]], reverse=True)
+    ws = ws[0:10]
+
+    featureIndexes = []
+    for i in range(len(w[0])):
+        if w[0][i] in ws or (-1) * w[0][i] in ws:
+            featureIndexes.append(i)
+
+    print "indexes: "
+    print featureIndexes
+    filters = []
+    for index in featureIndexes:
+        print allTestData[0][0].keys()[index]
+        filters.append(allTestData[0][0].keys()[index])
+
+
+
+    hamFeaturedTrain = [ (getFeaturesFiltered(vocab, h,filters), 0) for h in hamProcessedTrain]
+    spamFeaturedTrain = [ (getFeaturesFiltered(vocab, s,filters), 1) for s in spamProcessedTrain]
+
+    hamFeaturedTest = [ (getFeaturesFiltered(vocab, ha, filters), 0) for ha in hamProcessedTest]
+    spamFeaturedTest = [ (getFeaturesFiltered(vocab, sa, filters), 1) for sa in spamProcessedTest]
+
+
+    allTrainData = hamFeaturedTrain + spamFeaturedTrain
+    svmDataTrain = [d[0] for d in allTrainData]
+    svmAnsTrain = [d[1] for d in allTrainData]
+
     allTestData = hamFeaturedTest + spamFeaturedTest
     svmDataTest = [d[0] for d in allTestData]
     svmAnsTest = [d[1] for d in allTestData]
@@ -145,8 +226,9 @@ def main():
     svmTrainDataVectored = vec.fit_transform(svmDataTrain).toarray()
     svmTestDataVectored = vec.fit_transform(svmDataTest).toarray()
 
-    svmClass = svm.SVC(kernel = 'rbf', gamma=0.0005, C = 600)
-    print "fitting SVM"
+
+    svmClass = svm.SVC(kernel = 'linear', gamma=0.0005, C = 600)
+    print "fitting SVM again" 
     test_predict = svmClass.fit(svmTrainDataVectored, svmAnsTrain).predict(svmTestDataVectored)
 
     cm = confusion_matrix(svmAnsTest, test_predict)
